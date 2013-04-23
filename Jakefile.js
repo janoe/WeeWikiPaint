53dca8f45e638901587db266ec351967ca554403
José Antonio Noé <jose.antonio.noe@gmail.com>
2013-04-23 13:43:44 +0200

    Initial support por lint (JSHint)

--- Jakefile.js
+++ Jakefile.js
@@ -1,8 +1,21 @@
-desc("Example!");
-task("example", ["dependency"], function() {
-        console.log("example task");
-});
+/*global desc, task, jake, fail, complete */
 
-task("dependency", function() {
-        console.log("dependency");
+"use strict";
+
+task("default", ["lint"]);
+
+desc("Lint everything");
+task("lint", [], function() {
+    var lint = require("./build/lint/lint_runner.js");
+
+    var files = new jake.FileList();
+    files.include("**/*.js");
+    files.exclude("node_modules");
+    files.exclude("build");
+
+    var options = {
+        node: true
+    };
+
+    lint.validateFileList(files.toArray(), options, {});
 });
