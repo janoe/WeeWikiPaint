95bdca1c57a94a28d3c6373b6f7bd27ea6a85a15
OpenShift guest <jose.antonio.noe@gmail.com>
2013-05-23 13:31:51 -0400

    Capitulo 12, hacemos que el test arranque y pare el servidor.

--- test/server/_server_test.js
+++ test/server/_server_test.js
@@ -3,10 +3,26 @@
 var http = require("http");
 var server = require("../../src/server/server.js");
 
+exports.tearDown = function(done){
+    console.log("begin Teardown");
+    server.stop(function(){
+        console.log("Servidor parado");
+    }); //Paramos el servidor
+    console.log("End teardown");
+    done();
+};
+
 exports.testHttpServerIsWorking = function(test){
-    server.start();
-    http.get("http://" + process.env.IP + ":" + process.env.PORT,function(){
-        server.stop(); //Si todo Ok paramos el servidor
+    console.log("Iniciamos servidor");
+    server.start(function(){
+        console.log("Servidor iniciado");
     });
-    test.done();
+    console.log("Llamamos al http");
+    http.get("http://" + process.env.IP + ":" + process.env.PORT,function(response){
+        console.log("testHttpServerIsWorking ha ido Ok" + response.status);
+        response.on("data",function(){
+            console.log("Data processing");
+            test.done();
+        });
+    });
 };
